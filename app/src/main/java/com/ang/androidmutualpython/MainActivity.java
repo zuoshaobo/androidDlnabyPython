a5b0dab94ec98811eafc6d9cd7c67068efc5dbff
charles <charles@xx.com>
2021-11-25 08:28:32 +0800

    first version

---
+++ app/src/main/java/com/ang/androidmutualpython/MainActivity.java
@@ -0,0 +1,149 @@
+package com.ang.androidmutualpython;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.res.AssetManager;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+import com.srplab.www.starcore.StarCoreFactory;
+import com.srplab.www.starcore.StarCoreFactoryPath;
+import com.srplab.www.starcore.StarObjectClass;
+import com.srplab.www.starcore.StarServiceClass;
+import com.srplab.www.starcore.StarSrvGroupClass;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+    public StarSrvGroupClass srvGroup;
+    public static MainActivity Host;
+    private boolean isCopy;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        this.Host = this;
+    }
+
+
+    public void click2(View view) {
+        File destDir = new File("/data/data/" + getPackageName() + "/files");
+        if (!destDir.exists())
+            destDir.mkdirs();
+        java.io.File python2_7_libFile = new java.io.File("/data/data/" + getPackageName() + "/files/python3.9.zip");
+        if (!python2_7_libFile.exists()) {
+            try {
+                copyFile(this, "python3.9.zip", null);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        try {
+
+            copyFile(this, "math.cpython-39.so", null);
+            copyFile(this, "select.cpython-39.so", null);
+            copyFile(this, "_socket.cpython-39.so", null);
+            copyFile(this, "_struct.cpython-39.so", null);
+            copyFile(this, "binascii.cpython-39.so", null);
+            copyFile(this, "zlib.cpython-39.so", null);
+            copyFile(this, "test_calljava.py", null);
+
+            isCopy = true;
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+    public void click(View view) {
+
+        if (true) {
+            /*----load test.py----*/
+            String pystring = null;
+            try {
+                AssetManager assetManager = getAssets();
+                InputStream dataSource = assetManager.open("MathTest.py");
+                int size = dataSource.available();
+                byte[] buffer = new byte[size];
+                dataSource.read(buffer);
+                dataSource.close();
+                pystring = new String(buffer);
+            } catch (IOException e) {
+                System.out.println(e);
+            }
+
+            try {
+                //--load python34 core library first;
+                System.load(this.getApplicationInfo().nativeLibraryDir + "/libpython3.9.so");
+                //System.load(this.getApplicationInfo().nativeLibraryDir + "/_socket.cpython-39.so");
+            } catch (UnsatisfiedLinkError ex) {
+                System.out.println(ex.toString());
+            }
+
+            /*----init starcore----*/
+            StarCoreFactoryPath.StarCoreCoreLibraryPath = this.getApplicationInfo().nativeLibraryDir;
+            StarCoreFactoryPath.StarCoreShareLibraryPath = this.getApplicationInfo().nativeLibraryDir;
+            StarCoreFactoryPath.StarCoreOperationPath = "/data/data/" + getPackageName() + "/files";
+
+            StarCoreFactory starcore = StarCoreFactory.GetFactory();
+            Integer s = new Random().nextInt(100);
+            StarServiceClass service = starcore._InitSimple("MathTest" + s, "123", 0, 0);
+            srvGroup = (StarSrvGroupClass) service._Get("_ServiceGroup");
+            service._CheckPassword(false);
+
+            /*----run python code----*/
+            srvGroup._InitRaw("python39", service);
+            StarObjectClass python = service._ImportRawContext("python", "", false, "");
+
+            StarObjectClass pythonSys = python._GetObject("sys");
+            StarObjectClass pythonPath = (StarObjectClass) pythonSys._Get("path");
+
+            Log.d("aaaa", "/data/data/" + getPackageName() + "/files/python3.9.zip");
+            pythonPath._Call("insert", 0, "/data/data/" + getPackageName() + "/files/python3.9.zip");
+            pythonPath._Call("insert", 0, this.getApplicationInfo().nativeLibraryDir);
+            pythonPath._Call("insert", 0, "/data/data/" + getPackageName() + "/files");
+            pythonPath._Call("import", "_socket");
+
+            python._Call("execute", pystring);
+            Object object = python._Call("projector","/data/data/com.ang.androidmutualpython/files/xx.jpg");
+            Toast.makeText(this,object.toString(),Toast.LENGTH_LONG).show();
+
+            String CorePath = "/data/data/" + getPackageName() + "/files";
+            python._Set("JavaClass", CallBackClass.class);
+            service._DoFile("python", CorePath + "/test_calljava.py", "");
+        }else {
+            Toast.makeText(this, "请点击复制python库..按钮，完成拷贝！", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    private void copyFile(Activity c, String Name, String desPath) throws IOException {
+        File outfile = null;
+        if (desPath != null)
+            outfile = new File("/data/data/" + getPackageName() + "/files/" + desPath + Name);
+        else
+            outfile = new File("/data/data/" + getPackageName() + "/files/" + Name);
+        //if (!outfile.exists()) {
+        outfile.createNewFile();
+        FileOutputStream out = new FileOutputStream(outfile);
+        byte[] buffer = new byte[1024];
+        InputStream in;
+        int readLen = 0;
+        if (desPath != null)
+            in = c.getAssets().open(desPath + Name);
+        else
+            in = c.getAssets().open(Name);
+        while ((readLen = in.read(buffer)) != -1) {
+            out.write(buffer, 0, readLen);
+        }
+        out.flush();
+        in.close();
+        out.close();
+    }
+
+}
